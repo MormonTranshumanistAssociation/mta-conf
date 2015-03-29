6582332519eeac8dfcf7974622d476ecc500d86f
Nathan Hadfield <hadfieldn@gmail.com>
2015-03-29 04:18:37 -0600

    Basic skeleton and styling

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,13 +5,15 @@
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
-  app.appName = '2015 MTA Conference';
+  app.appName = '#mtaconf 2015';
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
   app.addEventListener('template-bound', function() {
     console.log('Ready!');
   });
+
+  app.selected = 0;
 
 // wrap document so it plays nice with other libraries
 // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
